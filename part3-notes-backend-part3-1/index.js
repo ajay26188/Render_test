dddee7ed11f5498483e5fa01789f4703160da0c2
ajay26188 <ajaysah52@gmail.com>
2025-05-28 16:31:19 +0530

    last commit

--- part3-notes-backend-part3-1/index.js
+++ part3-notes-backend-part3-1/index.js
@@ -31,7 +31,7 @@ app.use(express.static('dist'))
 
 
 app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+  response.send('<h1>Hello World</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
